9074ccdee94ab6edcaec7a65679a65ac020feeec
Mehul <318097@gmail.com>
2019-10-16 13:12:07 +0530

    Refactor: Sign in & Sign up Components

--- store-client/src/app.js
+++ store-client/src/app.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Header from './components/layout/header.component';
 import Home from './components/home.component';
-import Auth from './components/auth/auth.component';
+import SignIn from './components/auth/sign-in.component';
+import SignUp from './components/auth/sign-up.component';
 
 import './app.scss';
 
-const App = () => {
+const App = ({ currentUser }) => {
   return (
     <div className="app">
       <Header />
       <div>
         <Switch>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/" component={Auth} />
+          <Route exact path="/sign-in" render={() => currentUser ? <Redirect to="/home" /> : <SignIn />} />
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
+export default connect(mapStateToProps)(App);
