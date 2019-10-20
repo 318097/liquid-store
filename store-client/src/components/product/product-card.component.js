636f50fab914b2b4bf95a4942457ffd33487e48f
Mehul <318097@gmail.com>
2019-10-20 23:38:07 +0530

    Update: Fetch random images using unsplash api

--- store-client/src/components/product/product-card.component.js
+++ store-client/src/components/product/product-card.component.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux';
 
 import { addToCart } from '../../store/cart/cart.actions';
 import './product-card.styles.scss';
 
 const ProductCard = ({ item, addToCart }) => {
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.unsplash.com/photos/random?client_id=f2e1b0faaad5e5c469fc832355985d7416627cacb4bf1feacee1f60159c4d24b&orientation=squarish')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setUrl(data.urls.small);
+      })
+  }, []);
+
   return (
     <div className="card">
       <div className="image">
-        <img alt="product" src="https://source.unsplash.com/random/200x250" />
+        <img alt="product" src={url} />
       </div>
       <div className="info">
         <span>
